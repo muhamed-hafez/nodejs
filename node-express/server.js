896dabebab94b0e80fae8d9a1ad64fe64a4b3156
Muhamed Hafez <muhamed.hafez@inovaeg.com>
2016-11-09 19:10:00 +0200

    serving static files using express.static middleware

--- node-express/server.js
+++ node-express/server.js
@@ -1,19 +1,15 @@
 var express = require("express");
 var http = require("http");
+var morgan = require("morgan");
 
 var hostname = "localhost";
 var port = 3000;
 
 var app = express();
 
-app.use(function(req, res, next) {
-    console.log(req.headers);
-    res.writeHead(200, {"Content-Type" : "text/html"});
-    res.end("<html><body><h1>Hello, world!</h1></body></html>");
-});
+app.use(morgan("dev"));
+app.use(express.static("./public"));
 
-var server = http.createServer(app);
-
-server.listen(port, hostname, function() {
+app.listen(port, hostname, function() {
     console.log(`Server is running at http://${hostname}:${port}/`);
 })
