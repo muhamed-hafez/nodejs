15072312ca4e21ad0c2399504477c8d1ab5449a8
Muhamed Hafez <muhamed.hafez@inovaeg.com>
2016-11-09 15:03:03 +0200

    Creating a simple http server

--- Shape.js
+++ Shape.js
@@ -6,6 +6,7 @@ var notes = [
   {id : 1, creater : "Hafez", content : "Node is good"},
   {id : 2, creater : "Hafez", content : "Hello, world!"},
   {id : 3, creater : "Hafez", content : "Playing around with Node.js"}
+  {id : 4, creater : "Hafez", content : "See the orange of modified :)"}
 ];
 
 app.get("/notes", function(req, res) {
