23b1509f64f0671fb674f7e4739671a08c0ecdc4
Muhamed Hafez <muhamed.hafez@gmail.com>
2016-11-10 12:44:42 +0200

    Add body parser

--- Shape.js
+++ Shape.js
@@ -1,20 +1,23 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 
 var notes = [
   {id : 1, creater : "Hafez", content : "Node is good"},
   {id : 2, creater : "Hafez", content : "Hello, world!"},
-  {id : 3, creater : "Hafez", content : "Playing around with Node.js"}
+  {id : 3, creater : "Hafez", content : "Playing around with Node.js"},
   {id : 4, creater : "Hafez", content : "See the orange of modified :)"}
 ];
+
+app.use(bodyParser.json());
 
 app.get("/notes", function(req, res) {
   res.json(notes);
 })
 
 app.post("/add_note", function(req, res) {
-  console.log(req);
+  console.log(req.body);
   notes.push(req.body);
   res.json(notes);
 })
