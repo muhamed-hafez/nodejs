14ef15208955fff62b7302e11e86962335d288ee
Muhamed Hafez <muhamed.hafez@gmail.com>
2016-11-13 12:46:41 +0200

    Creating a db operation file and using it to connect to the mongodb server

--- testingMongoDB.js
+++ testingMongoDB.js
@@ -1,25 +1,31 @@
 var mongoClient = require("mongodb").MongoClient;
 var assert = require("assert");
+var dboper = require("./operations")
 
 var url = "mongodb://localhost:27017/conFusion";
 
 mongoClient.connect(url, function(err, db) {
     assert.equal(err, null);
     console.log("Connect correctly to server");
-    var collection = db.collection("dishes");
-    collection.insertOne({"name" : "Pizza", "discription" : "The best meal"}
-        , function(err, result) {
-            assert.equal(err, null);
-            console.log("After insert : ");
-            console.log(result.ops);
-            collection.find({}).toArray(function(err, docs) {
-                assert.equal(err, null);
-                console.log("Found : ");
-                console.log(docs);
-                db.dropCollection("dishes", function(err, result) {
-                    assert.equal(err, null);
-                    db.close();
+
+    dboper.insertDocument(db, {name : "Fatta", description : "An egyptian meal"}, "dishes", function(result) {
+        console.log(result.ops);
+
+        dboper.findDocument(db, "dishes", function(docs) {
+            console.log(docs);
+
+            dboper.updateDocument(db, {name : "Fatta"}, {description : "The best meal ever"}, "dishes", function(result) {
+                console.log(result.result);
+
+                dboper.findDocument(db, "dishes", function(docs) {
+                    console.log(docs);
+
+                    db.dropCollection("dishes", function(result) {
+                        console.log("drop collection result : " + result);
+                        db.close();
+                    });
                 });
             });
+        });
     });
 });
