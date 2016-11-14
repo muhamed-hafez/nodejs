39f3e81ea9a7b17fcd1ed1a14d52bb5f4b1d43fb
Muhamed Hafez <muhamed.hafez@gmail.com>
2016-11-14 18:06:18 +0200

    Applying basic authorization

--- node-express/server.js
+++ node-express/server.js
@@ -8,7 +8,33 @@ var port = 3000;
 var app = express();
 
 app.use(morgan("dev"));
+
+app.use(function(req, res, next) {
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    var username, password;
+    if(authHeader) {
+        var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+        username = auth[0];
+        password = auth[1];
+    }
+
+    if(username === 'admin' && password === 'password') {
+        next();
+    }
+    else {
+        var error = new Error('You are not authenticated!');
+        error.status = 401;
+        next(error);
+    }
+});
+
 app.use(express.static("./public"));
+
+app.use(function(err, req, res, next) {
+    res.writeHead(err.status || 500, {'Content-Type' : 'text/plain', 'WWW-Authenticate' : 'Basic'});
+    res.end(err.message);
+});
 
 app.listen(port, hostname, function() {
     console.log(`Server is running at http://${hostname}:${port}/`);
